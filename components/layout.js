05fd1769b8ccaf87006baf400a57e3138755a308
Seian <kestinio.student@ua.edu.ph>
2026-08-18 10:52:33 +0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Karl Tinio';
+const name = 'Tinio, Karl Emmmanuel S.';
 export const siteTitle = 'Next.js Sample Website';
  
 export default function Layout({ children, home }) {
