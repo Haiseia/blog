10f8004594edbc0808cca04de3e9de443dd995f3
Seian <kestinio.student@ua.edu.ph>
2026-08-18 10:49:35 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>August 13, 2026</p>
         <p>
           Greetings, I'm Karl. My hobbies include playing and learning things. As for my favorite music, it's difficult to decide but if I had to choose one, then it'd be [The Shimmering Voyage Vol. 6] - Transient Tranquility.
           I'm not exactly a picky eater, though Sisig albeit a little unhealthy in excessive amounts is probably my favorite food. I look forwad to seeing myself be a stable man with very good time schedule and revenue/salary in 5 years.
