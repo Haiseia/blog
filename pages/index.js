f95f75241a3a94763f8fcd6330f7730c772d4e57
Seian <kestinio.student@ua.edu.ph>
2026-08-18 10:53:56 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Greetings, I'm Karl. My hobbies include playing and learning things. As for my favorite music, it's difficult to decide but if I had to choose one, then it'd be [The Shimmering Voyage Vol. 6] - Transient Tranquility.
+          Greetings, I'm Karl. My hobbies include playing and learning things. As for my favorite music, it's difficult to decide but if I had to choose one, then it'd be [The Shimmering Voyage Vol. 6 - Transient Tranquility].
           I'm not exactly a picky eater, though Sisig albeit a little unhealthy in excessive amounts is probably my favorite food. I look forwad to seeing myself be a stable man with very good time schedule and revenue/salary in 5 years.
         </p>
         <p>
